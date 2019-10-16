f5acd05d0429fc60de0f3683310078071ffc146f
Dan <dsinger.sax@gmail.com>
2019-10-15 22:28:28 -0400

    Initial commit

---
+++ Source/SpaceFPS/SpaceFPS.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceFPS, "SpaceFPS" );
+ 
